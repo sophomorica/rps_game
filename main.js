57739c4205363e7f5ae5fa97390c5672a7604baf
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-28 17:31:10 -0600

    remove function works

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 var answerArray = ["rock", "paper", "scissors"];
 var userChoice;
-var choices = document.getElementsByClassName("choice")
-var userView = document.getElementById("user")
-var compView = document.getElementById("comp")
+var choices = document.getElementsByClassName("choice");
+var userView = document.getElementById("user");
+var compView = document.getElementById("comp");
 
-answerArray.forEach(function(answer){
-console.log(answer)
-})
+answerArray.forEach(function(answer) {
+  console.log(answer);
+});
 
-for (var i = 0; i < choices.length; i++){
-  choices[i].addEventListener("click", function(){
-    userChoice = this.id 
-    switch(userChoice){
+for (var i = 0; i < choices.length; i++) {
+  choices[i].addEventListener("click", function() {
+    removeCards()
+
+    userChoice = this.id;
+
+    let image = document.createElement("img");
+    userView.appendChild(image);
+    switch (userChoice) {
       case "rock":
-      userView.classList = "rock"
-      break
+        image.src = `/images/rock.jpeg`;
+        break;
       case "paper":
-      userView.classList = "paper"
-      break
+        image.src = `/images/paper.jpeg`;
+        break;
       case "scissors":
-      userView.classList = "scissors"
-
+        image.src = `/images/scissors.png`;
     }
-
-  })
+  });
 }
 
+const removeCards = () => {
+  let removeDiv = document.querySelector(".result");
+  while (removeDiv.firstChild) {
+    removeDiv.removeChild(removeDiv.firstChild);
+  }
+};
